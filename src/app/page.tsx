212ae71eaf204bab47cab4363060e78d6cdebfc0
smartCode2018 <smartcode.kc@gmail.com>
2025-11-14 12:45:32 +0100

    02: database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import prisma from "@/lib/db";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const page = async () => {
+  const users = await prisma.user.findMany();
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      {JSON.stringify(users)}
+    </div>
+  );
 };
 
 export default page;
