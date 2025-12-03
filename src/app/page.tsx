70bcce335197f4d97bec7065e9f7ce1b19faa672
smartCode2018 <smartcode.kc@gmail.com>
2025-12-03 21:39:05 +0100

    07: ai providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,30 @@ import {toast} from "sonner";
 const page = () => {
   const trpc = useTRPC();
   const queryClient = useQueryClient();
-  const {data} = useQuery(trpc.getWorkfows.queryOptions());
+  const {data} = useQuery(trpc.getWorkflows.queryOptions());
+
+  const testAi = useMutation(
+    trpc.testAi.mutationOptions({
+      onSuccess: () => {
+        toast.success("Job queued");
+      },
+    })
+  );
+
   const create = useMutation(
     trpc.createWorkflow.mutationOptions({
       onSuccess: () => {
         toast.success("Job queued");
-        queryClient.invalidateQueries(trpc.getWorkfows.queryOptions());
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
       },
     })
   );
 
   return (
     <div className="min-h-screen min-w-screen flex flex-col gap-y-6 p-4 items-center justify-center">
+      <Button disabled={testAi.isPending} onClick={() => testAi.mutate()}>
+        Test AI
+      </Button>
       <div>{JSON.stringify(data)}</div>
       <Button disabled={create.isPending} onClick={() => create.mutate()}>
         Create workflow
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,17 @@
 import {inngest} from "@/inngest/client";
 import {createTRPCRouter, protectedProcedure} from "../init";
 import prisma from "@/lib/db";
+
 export const appRouter = createTRPCRouter({
-  getWorkfows: protectedProcedure.query(({ctx}) => {
+  testAi: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "execute/ai",
+    });
+
+    return {success: true, message: "AI Execution triggered"};
+  }),
+
+  getWorkflows: protectedProcedure.query(({ctx}) => {
     return prisma.workflow.findMany();
   }),
   createWorkflow: protectedProcedure.mutation(async () => {
